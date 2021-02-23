1f11679dfb7239a214bf417829e0d384b598cb80
Bert Peters <bert.peters@channable.com>
2021-02-23 12:07:45 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/bert-training/network/google"
+  version = "2.5.0"
+  
+  network_name = "gaurav-network"
+  project_id = var.project
+
+  subnets = [
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
+  ]
+}
